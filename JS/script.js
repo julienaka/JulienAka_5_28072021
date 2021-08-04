4169bc9084ccedd36b3206032afadeeac622dd14
Julien AKA <julienaka.96@gmail.com>
2021-08-04 18:25:26 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,38 @@
+const productSection = document.getElementById('widthMain')
+
+//récupération des données de l'api puis incrustation dynamique dans l'HTML
+fetch('http://localhost:3000/api/cameras')
+	.then(res => {
+		if (res.ok) {
+			res.json().then(data => {
+				console.log(data);
+				for(let i in data){
+
+					const newDiv = document.createElement("div");
+					newDiv.classList.add('product');
+					newDiv.setAttribute('id', data[i]._id);
+					const newA = document.createElement("a");
+					//passage des données pas lurl a la page card lors du click
+					newA.href = "HTML/card.html?id="+data[i]._id;
+					const secondDiv = document.createElement("div");
+					secondDiv.classList.add('absoluteProduct');
+					const newH2 = document.createElement("h2");
+					newH2.innerHTML = data[i].name;
+					const newP = document.createElement("p");
+					newP.innerHTML = data[i].price/100 + "€";
+					const newImg = document.createElement("img");
+					newImg.src = data[i].imageUrl;
+
+					productSection.appendChild(newDiv);
+					newDiv.appendChild(newA);
+					newA.appendChild(secondDiv);
+					secondDiv.appendChild(newH2);
+					secondDiv.appendChild(newP);
+					newA.appendChild(newImg);
+				}
+			})
+		}else {
+			console.log('Error');
+		}
+	})
+© 2021 GitHub, Inc.
